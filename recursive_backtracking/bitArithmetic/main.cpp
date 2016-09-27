63ec5f5412ac62b51d4d8e2768aaf93baeddef0e
Gabriel Berthling-Hansen <gabrielbhansen@hotmail.com>
2016-09-28 01:38:07 +0200

    It can now find any all solutions to any N tested up to n=14

--- recursive_backtracking/bitArithmetic/main.cpp
+++ recursive_backtracking/bitArithmetic/main.cpp
@@ -27,7 +27,7 @@ void drawBoard(){
         }
         std::cout << " ";
     }
-    std::cout << "]" << "\n";
+    std::cout << "]" << "       # of solutions: " << solutions << "\n";
 }
 
 bool solve(int queenIndex, unsigned int firstRow){
@@ -42,16 +42,15 @@ bool solve(int queenIndex, unsigned int firstRow){
     int n = 0;
     unsigned int lsb;
     t = filled;
-    unsigned int tmp;
     for(;;){
         if(t != 0){
             lsb = filled & ~(t & -t);
             steps[n] = t;
             n++;
-            std::cout << "****************** Added T ******************" << "\n";
-            std::cout << "T: " << t << "\n";
+//            std::cout << "****************** Added T ******************" << "\n";
+//            std::cout << "T: " << t << "\n";
 
-            std::cout << tmpRow[n - 1] << ", " << tmpLeft[n - 1] << ", " << tmpRight[n - 1] << "\n";
+//            std::cout << tmpRow[n - 1] << ", " << tmpLeft[n - 1] << ", " << tmpRight[n - 1] << "\n";
             //Set the queens position to zero
             tmpRow[n] = tmpRow[n - 1] & lsb;
             tmpLeft[n] = tmpLeft[n - 1] & (filled & lsb);
@@ -61,59 +60,37 @@ bool solve(int queenIndex, unsigned int firstRow){
             tmpRight[n] = filled & (tmpRight[n] >> 1 | (1 << size_m1));
 
             t = tmpRow[n] & tmpLeft[n] & tmpRight[n];
-            std::cout << tmpRow[n] << ", " << tmpLeft[n] << ", " << tmpRight[n] << "\n";
-            std::cout << "t: " << t << "\n";
+//            std::cout << tmpRow[n] << ", " << tmpLeft[n] << ", " << tmpRight[n] << "\n";
+//            std::cout << "t: " << t << "\n";
         }else{
-            std::cout << "================== T is zero ==================" << "\n";
-            std::cout << "T: " << t << "\n";
+            if(n == 0)
+                return true;
+//            std::cout << "================== T is zero ==================" << "\n";
+//            std::cout << "T: " << t << "\n";
             n --;
             t = steps[n];
             lsb = filled & ~(t & -t);
-            std::cout << "T before lsb: " << t << "\n";
+//            std::cout << "T before lsb: " << t << "\n";
             t &= lsb;
-            std::cout << "T is now: " << t << "\n";
+//            std::cout << "T is now: " << t << "\n";
         }
 
         if(n == size){
-            std::cout << "Solution has been found" << "\n";
+            bits.clear();
             for(int i = 0; i < n; i++){
                 t = steps[i];
                 lsb = filled & ~(t & -t);
                 bits.push_back(lsb);
             }
             drawBoard();
-            break;
+            solutions ++;
+
+            n --;
+            lsb = filled & ~(t & -t);
+            t &= lsb;
         }
-        std::cout << "N: " << n << "\n";
+//        std::cout << "N: " << n << "\n";
     }
-/*
-int row = collision(queenIndex, firstRow);
-if(row == -1)
-    return false;
-bits[queenIndex] = row;
-
-if(queenIndex == size_m1){
-    return false;
-}
-
-if(solve(queenIndex + 1, filled))
-    return true;
-
-if(bits[queenIndex] == endMarker){
-    return queenIndex == 0;
-}
-
-//Shift the position by 1 and set the pushed position to 1
-firstRow = bits[queenIndex] << 1 | 1;
-//Set every position above the shifted position to 1 and everything else to 0
-//e.g: Position: 11101 becomes: 11011 which becomes 11000
-firstRow = filled - ((~firstRow & -firstRow) - 1);
-
-return solve(queenIndex, firstRow);
-
-
-*/
-
 }
 
 
@@ -126,9 +103,10 @@ int main(int argc, char* argv[]){
     for(int i = 0; i < size; i++){
 //        bits.push_back(filled);
         positions.push_back(pow(2, i));
-        std::cout << pow(2, i) << "\n";
+//        std::cout << pow(2, i) << "\n";
     }
 
+    std::cout << "Begin" << "\n";
     solve(0, filled);
 
     std::cout << "Backtracking: " << size << " queens" << "\n";

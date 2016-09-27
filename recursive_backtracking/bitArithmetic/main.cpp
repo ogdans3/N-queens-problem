516f252864db972efbe32ef631562fec35e88fd2
Gabriel Berthling-Hansen <gabrielbhansen@hotmail.com>
2016-09-28 01:53:18 +0200

    With -Ofast this does n=16 in 15.787s

--- recursive_backtracking/bitArithmetic/main.cpp
+++ recursive_backtracking/bitArithmetic/main.cpp
@@ -5,30 +5,15 @@
 int size;
 int size_m1;
 int solutions;
-unsigned int endMarker;
+int n = 0;
+int n_m1 = -1;
+
 unsigned int filled;
-unsigned int q;
+unsigned int lsb;
 unsigned int t;
-unsigned int bit;
+unsigned int first = (1 << 0);
+unsigned int last;
 
-std::vector<unsigned int> bits;
-std::vector<int> positions;
-
-void drawBoard(){
-    std::cout << "[ ";
-    for(int i = 0; i < bits.size(); i++){
-        bit = bits[i];
-        bit = (filled & ~bit);
-        for(int q = 0; q < size; q++){
-            if(positions[q] == bit){
-                std::cout << q + 1;
-                break;
-            }
-        }
-        std::cout << " ";
-    }
-    std::cout << "]" << "       # of solutions: " << solutions << "\n";
-}
 
 bool solve(int queenIndex, unsigned int firstRow){
     unsigned int steps[size];
@@ -39,57 +24,44 @@ bool solve(int queenIndex, unsigned int firstRow){
     tmpRow[0] = filled;
     tmpLeft[0] = filled;
     tmpRight[0] = filled;
-    int n = 0;
-    unsigned int lsb;
     t = filled;
     for(;;){
+        lsb = filled & ~(t & -t);
         if(t != 0){
-            lsb = filled & ~(t & -t);
             steps[n] = t;
-            n++;
-//            std::cout << "****************** Added T ******************" << "\n";
-//            std::cout << "T: " << t << "\n";
+            n ++;
+            n_m1 ++; //Variable to speed up processing
 
-//            std::cout << tmpRow[n - 1] << ", " << tmpLeft[n - 1] << ", " << tmpRight[n - 1] << "\n";
-            //Set the queens position to zero
-            tmpRow[n] = tmpRow[n - 1] & lsb;
-            tmpLeft[n] = tmpLeft[n - 1] & (filled & lsb);
-            tmpLeft[n] = filled & (tmpLeft[n] << 1 | (1 << 0));
-
-            tmpRight[n] = tmpRight[n - 1] & (filled & lsb);
-            tmpRight[n] = filled & (tmpRight[n] >> 1 | (1 << size_m1));
+            tmpRow[n] = tmpRow[n_m1] & lsb;
+            tmpLeft[n] = (tmpLeft[n_m1] & lsb) << 1 | first;
+            tmpRight[n] = (tmpRight[n_m1] & lsb) >> 1 | last;
 
             t = tmpRow[n] & tmpLeft[n] & tmpRight[n];
-//            std::cout << tmpRow[n] << ", " << tmpLeft[n] << ", " << tmpRight[n] << "\n";
-//            std::cout << "t: " << t << "\n";
         }else{
+            //We did not find a new position
+
+            //We are at the first queen
+            //This means that we have completed the search, return
             if(n == 0)
                 return true;
-//            std::cout << "================== T is zero ==================" << "\n";
-//            std::cout << "T: " << t << "\n";
+            //We count down, this is basically the same as backtracking.
             n --;
+            n_m1 --; //Variable to speed up processing
+
             t = steps[n];
+            //Simply toggle the bit it was currently on as off
+            //so that we start the search on the next row
             lsb = filled & ~(t & -t);
-//            std::cout << "T before lsb: " << t << "\n";
             t &= lsb;
-//            std::cout << "T is now: " << t << "\n";
         }
 
         if(n == size){
-            bits.clear();
-            for(int i = 0; i < n; i++){
-                t = steps[i];
-                lsb = filled & ~(t & -t);
-                bits.push_back(lsb);
-            }
-            drawBoard();
             solutions ++;
 
-            n --;
-            lsb = filled & ~(t & -t);
             t &= lsb;
+            n --;
+            n_m1 --;
         }
-//        std::cout << "N: " << n << "\n";
     }
 }
 
@@ -98,15 +70,8 @@ int main(int argc, char* argv[]){
     sscanf(argv[1], "%d", &size);
     size_m1 = size - 1;
     filled = (1 << size) - 1;
-    endMarker =  filled >> 1;
+    last = (1 << size_m1);
 
-    for(int i = 0; i < size; i++){
-//        bits.push_back(filled);
-        positions.push_back(pow(2, i));
-//        std::cout << pow(2, i) << "\n";
-    }
-
-    std::cout << "Begin" << "\n";
     solve(0, filled);
 
     std::cout << "Backtracking: " << size << " queens" << "\n";
